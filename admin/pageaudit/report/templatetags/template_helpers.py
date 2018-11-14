3529a7b7a3a58ffb8be159da2dbb5cd0de5bd604
Michael Santelia <santelia@us.ibm.com>
2018-11-14 02:22:39 -0500

    Report list page. Need to investigate checkbox issue in FF only.

--- admin/pageaudit/report/templatetags/template_helpers.py
+++ admin/pageaudit/report/templatetags/template_helpers.py
@@ -10,16 +10,24 @@ def getTemplateHelpers(context):
     
     horizontalSpace = 'ph3 ph4-ns'
     rounded = 'br2'
-    commonButton = 'pointer br2 ba ph4 pv3 bg-animate border-box ' + rounded
+    commonButton = 'pointer dib mb3 br2 ba ph4 pv3 bg-animate border-box ' + rounded
+    smallButton = 'pointer dib mb3 br2 ba pa2 bg-animate border-box ' + rounded
+    bluePriButton = 'b--dark-blue bg-blue hover-bg-dark-blue white'
+    blueSecButton = 'b--blue bg-white hover-bg-blue blue hover-white link'
+    greenPriButton = 'b--dark-green bg-green hover-bg-dark-green white'
     
     return {
         'classes': {
-            'commonButton': commonButton,
-            'viewReport': commonButton + ' b--dark-green bg-green hover-bg-dark-green white',
-            'viewAll': commonButton + ' b--blue bg-white hover-bg-blue blue hover-white link',
-            'grid': 'w-100 pt2 ' + horizontalSpace,
+            'button': commonButton,
+            'smallButton': smallButton,
+            'bluePriButton': bluePriButton,
+            'blueSecButton': blueSecButton,
+            'greenPriButton': greenPriButton,
             'horizontalSpace': horizontalSpace,
+            'grid': horizontalSpace + ' w-100',
             'navItem': 'link near-white f6 f5-ns dib mr4 pv3 hover-green',
             'rounded': rounded,
+            'viewAll': commonButton + ' b--blue bg-white hover-bg-blue blue hover-white link',
+            'viewReport': commonButton + ' b--dark-green bg-green hover-bg-dark-green white',
         }
     }
