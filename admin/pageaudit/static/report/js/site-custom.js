2fc0f7312aa245a9d73dabb9eff0307a8e3f32fe
Michael Santelia <ecumike@gmail.com>
2018-11-23 12:50:06 -0500

    Added lighthouse-viewer

--- admin/pageaudit/static/report/js/site-custom.js
+++ admin/pageaudit/static/report/js/site-custom.js
@@ -140,5 +140,34 @@
 		return true;
 	};
 	
+	// Copied from Lighthouse-viewer app in Google Lighthouse repo:
+	// https://github.com/GoogleChrome/lighthouse/blob/master/lighthouse-core/report/html/renderer/report-ui-features.js#L353-L384 
+	//   Made adjustment for origin/view path and added to our global namespace.
+    PL.openTabAndSendJsonReport = function (reportJson, viewerPath) {
+        const VIEWER_ORIGIN = window.location;
+        // Chrome doesn't allow us to immediately postMessage to a popup right
+        // after it's created. Normally, we could also listen for the popup window's
+        // load event, however it is cross-domain and won't fire. Instead, listen
+        // for a message from the target app saying "I'm open".
+        const json = reportJson;
+        
+        window.addEventListener('message', function msgHandler(/** @type {Event} */ e) {
+            const messageEvent = /** @type {MessageEvent} */ (e);
+            
+            if (popup && messageEvent.data.opened) {
+                popup.postMessage({lhresults: json}, VIEWER_ORIGIN);
+                window.removeEventListener('message', msgHandler);
+            }
+        });
+        
+        // The popup's window.name is keyed by version+url+fetchTime, so we reuse/select tabs correctly
+        // @ts-ignore - If this is a v2 LHR, use old `generatedTime`.
+        const fallbackFetchTime = /** @type {string} */ (json.generatedTime);
+        const fetchTime = json.fetchTime || fallbackFetchTime;
+        const windowName = `${json.lighthouseVersion}-${json.requestedUrl}-${fetchTime}`;
+        const popup = window.open(`${viewerPath}`, windowName);
+    }
+
+	
 })(jQuery, PL);
 
